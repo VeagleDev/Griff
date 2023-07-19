7a956c3f98a4aedb87f723e31ffa58eacee5b429
pierrbt <pierre08bt@gmx.co.uk>
2023-07-19 15:09:55 +0200

    fix: api service config

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
 import useConfig from "../hooks/useConfig";
 
+// eslint-disable-next-line react-hooks/rules-of-hooks
 const { get } = useConfig();
 
 const api = axios.create({
-  baseURL: get("serverUrl") + "/api",
+  baseURL: await get("serverUrl"),
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
-    "Authorization": "Bearer " + get("token") || "",
+    Authorization: "Bearer " + (await get("token")) || "",
   },
 });
 
