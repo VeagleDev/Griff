e9e205dddeacfa31fd20dd76055141cde0806efe
pierrbt <pierre08bt@gmx.co.uk>
2023-07-24 13:07:53 +0200

    refactor: run format

--- src/utils/fileSize.util.ts
+++ src/utils/fileSize.util.ts
@@ -1,13 +1,13 @@
 export function byteToHumanSizeString(bytes: number) {
   const sizes = ["B", "KB", "MB", "GB", "TB"];
-  if (bytes == 0) return "0 Byte";
+  if (bytes === 0) return "0 Byte";
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString());
   return (bytes / Math.pow(1024, i)).toFixed(1).toString() + " " + sizes[i];
 }
 
 export function byteToUnitAndSize(bytes: number) {
   const units = ["B", "KB", "MB", "GB", "TB"];
-  if (bytes == 0) return { unit: "B", size: 0 };
+  if (bytes === 0) return { unit: "B", size: 0 };
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString());
 
   return {
