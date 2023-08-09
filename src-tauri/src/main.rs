708ed8eb296416a615bbfa9efc55c0fc167216ef
pierrbt <pierre08bt@gmx.co.uk>
2023-08-09 02:35:30 +0200

    chore: update dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,4 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 use tauri::WindowEvent;
-use tauri::api::process::{Command, CommandEvent};
 
 fn main() {
     tauri::Builder::default()
