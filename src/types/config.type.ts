11372349e3f9a737139bbfe9b719416fee536a41
pierrbt <pierre08bt@gmx.co.uk>
2023-10-27 13:45:07 +0200

    feat: add type verif

--- src/types/config.type.ts
+++ src/types/config.type.ts
@@ -1,15 +1,26 @@
 import { InstalledGame } from "./game.type";
+import { z } from "zod";
+
+export const DataConfigSchema = z.object({
+  serverUrl: z.string().min(1).startsWith("http"),
+  serverName: z.string(),
+  username: z.string().min(1),
+  token: z.string().min(1),
+  firstName: z.string().min(1),
+  email: z.string(),
+  installedGames: z.any()
+});
 
 interface DataConfig {
-  serverUrl: string;
-  serverName: string;
+ serverUrl: string;
+ serverName: string;
 
-  username: string;
-  token: string;
-  firstName: string;
+ username: string;
+ token: string;
+ firstName: string;
 
-  email: string;
-  installedGames: InstalledGame[];
+ email: string;
+ installedGames: InstalledGame[];
 }
 
 export default DataConfig;
