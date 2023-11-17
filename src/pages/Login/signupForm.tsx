1aebba48d84c78ac249f1c6867d8055f88fb10a4
pierrbt <pierre08bt@gmx.co.uk>
2023-11-17 21:43:10 +0100

    refactor: format

--- src/pages/Login/signupForm.tsx
+++ src/pages/Login/signupForm.tsx
@@ -1,16 +1,10 @@
-import { Button, Flex, Stack, TextInput, Title } from "@mantine/core";
-import { createFormContext } from "@mantine/form";
-import { SignupFormValues } from "../../types/login.type";
-import { getSignupFormSubmit } from "./submit";
-import {
-  TbServer,
-  TbUserCircle,
-  TbMail,
-  TbPassword,
-  TbFaceId,
-} from "react-icons/tb";
+import {Button, Flex, Stack, TextInput, Title} from "@mantine/core";
+import {createFormContext} from "@mantine/form";
+import {SignupFormValues} from "../../types/login.type";
+import {getSignupFormSubmit} from "./submit";
+import {TbFaceId, TbMail, TbPassword, TbServer, TbUserCircle,} from "react-icons/tb";
 
-import { Dispatch, SetStateAction, useState } from "react";
+import {Dispatch, SetStateAction, useState} from "react";
 
 const [FormProvider, useFormContext, useForm] =
   createFormContext<SignupFormValues>();
