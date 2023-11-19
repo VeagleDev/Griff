2f9af9e6668fbdf0cd3e85fab614b7fc6c2134dd
pierrbt <pierre08bt@gmx.co.uk>
2023-11-19 15:10:03 +0100

    fix: manager

--- src/services/manager.service.ts
+++ src/services/manager.service.ts
@@ -1,7 +1,6 @@
 import {modals} from "@mantine/modals";
 import {OnlineGame} from "../types/game.type";
 import UseConfig from "../hooks/useConfig";
-import {createClient} from 'a2c'
 
 export async function downloadGame(game: OnlineGame) {
   const gameName = game.name;
@@ -37,17 +36,6 @@ export async function downloadGame(game: OnlineGame) {
       size: game.size,
     });
 
-    const aria2 = createClient({
-      url: 'ws://localhost:6800/jsonrpc', // defaultValue
-    })
-
-    const gid = await aria2.addUri(["magnet:?xt=urn:btih:95A67B258AA9785D9C5273349F3E5E8BDE8686B2&dn=Frigato%3A+Shadows+of+the+Caribbean+%28%2B+Bonus+Soundtrack%2C+MULTi27%29+%5BFitGirl+Repack%5D&tr=udp%3A%2F%2Fopentor.net%3A6969&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.gbitt.info%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.ccp.ovh%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.ccp.ovh%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&tr=https%3A%2F%2Ftracker.tamersunion.org%3A443%2Fannounce&tr=http%3A%2F%2Fopen.acgnxtracker.com%3A80%2Fannounce&tr=http%3A%2F%2Fopen.acgtracker.com%3A1096%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce"],
-      {dir: "C:\\Users\\pierr\\Downloads\\yessir"})
-
-    setInterval(async () => {
-      const status = await aria2.tellStatus(gid)
-      console.log(status)
-    }, 1000);
 
 
     await set("installedGames", installedGames);
